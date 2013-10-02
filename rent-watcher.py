9a8c4d2dc2873bbac9eb091290034b0ea092bc6f
Gabi Nagy <gabrian.nagy@gmail.com>
2013-10-02 12:46:05 +0300

    Moved code into function

--- rent-watcher.py
+++ rent-watcher.py
@@ -4,25 +4,35 @@
 import requests
 import bs4
 
-r = requests.get("http://localhost/da.html")
+def get_blitz(start_url, results, paginate=True):
+	urls = [start_url]
+	while len(urls) > 0:
+		url = urls.pop()
+		r = requests.get(url)
+		soup = bs4.BeautifulSoup(r.text, "html.parser")
+		items = soup.find("ul", {"class": "productList"}).find_all("li")
 
-soup = bs4.BeautifulSoup(r.text, "html.parser")
-items = soup.find("ul", {"class": "productList"}).find_all("li")
-results = {}
+		for item in items:
+			url = item.find("a")["href"]
+			name = unicode(item.find("span", {"class": "prodName"}).string).strip()
+			location = unicode(item.find("span", {"class": "sgreen"}).next_sibling.string).strip()
+			price = unicode(item.find("span", {"class": "prodPret"}).string).strip()
+			if url not in results:
+				results[url] = {
+					"name": name,
+					"location": location,
+					"price": price,
+				}
 
-for item in items:
-	url = item.find("a")["href"]
-	name = unicode(item.find("span", {"class": "prodName"}).string).strip()
-	location = unicode(item.find("span", {"class": "sgreen"}).next_sibling.string).strip()
-	price = unicode(item.find("span", {"class": "prodPret"}).string).strip()
-	if url not in results:
-		results[url] = {
-			"name": name,
-			"location": location,
-			"price": price,
-		}
+		if paginate:
+			try:
+				next_page = soup.find("div", {"class": "pageNav"}).find("a", {"class": "active"}).parent.next_sibling.find("a")["href"]
+			except Exception:
+				next_page = None
+			if next_page != None:
+				urls.append(next_page)
 
-try:
-	next_page = soup.find("div", {"class": "pageNav"}).find("a", {"class": "active"}).parent.next_sibling.find("a")["href"]
-except Exception:
-	next_page = None
+	return results
+
+import pprint
+pprint.pprint(get_blitz("http://localhost/da.html", {}))
