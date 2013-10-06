77917ca91d749eee997711a6b7f7336c279c99ba
Viorel Stirbu <viorels@gmail.com>
2013-10-06 22:29:49 +0300

    send html email

--- rent-watcher.py
+++ rent-watcher.py
@@ -6,46 +6,12 @@ import bs4
 import pickle
 import datetime
 import os
+import smtplib
 import time
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-config = {
-	"blitz": {
-		"start_url": 'http://www.blitz-imobiliare.ro/inchirieri-apartamente-cluj/searchId=85453',
-		"items": 'soup.find("ul", {"class": "productList"}).find_all("li")',
-		"url": 'item.find("a")["href"]',
-		"name": 'unicode(item.find("span", {"class": "prodName"}).string).strip()',
-		"location": 'unicode(item.find("span", {"class": "sgreen"}).next_sibling.string).strip()',
-		"price": 'unicode(item.find("span", {"class": "prodPret"}).string).replace(u"€/luna", "").strip()',
-		"next_url": 'soup.find("div", {"class": "pageNav"}).find("a", {"class": "active"}).parent.next_sibling.find("a")["href"]',
-	},
-	"welt": {
-		"start_url": 'http://www.weltimobiliare.ro/search?to=rent&type=a1+a2&hood=1+19+26+48+49+32+50+51+52&price-min=150&price-max=200',
-		"items": 'soup.find_all("li", {"class": "estate-listing"})',
-		"url": '"http://www.weltimobiliare.ro" + item.find("h2").find("a")["href"]',
-		"name": 'unicode(item.find("ul", {"class": "details"}).find("li").find("b").string).strip()',
-		"location": 'unicode(item.find("ul", {"class": "details"}).find("li").next_sibling.next_sibling.find("b").next_sibling.string).replace(":", "").strip()',
-		"price": 'unicode(item.find("ul", {"class": "details"}).find("span", {"class": "price"}).find("b").string).replace(u"Â €/luna", "").strip()',
-		"next_url": '"http://www.weltimobiliare.ro" + soup.find("li", {"class": "page selected"}).next_sibling.next_sibling.find("a")["href"]',
-	},
-	"edil": {
-		"start_url": 'http://www.edil.ro/actions/cautare_avansata.php?loc=CJ_00&tip_imob=1&tip_tranzactie=2&cam_min=1&cam_max=2&pret_min=150&pret_max=200&oferte_recomandate=0&exclus_demisol=demisol&exclus_parter=parter&exclus_mansarda=&exclus_ultim_etaj=ultim_etaj&st_limit=0&l=ro&monitorizare=1',
-		"items": 'soup.find_all("div", {"class": "line-content"})',
-		"url": '"http://www.edil.ro/" + item.find("div", {"class": "line-content-img"}).find("a")["href"]',
-		"name": 'unicode(item.find("div", {"class": "line-content-details"}).find_all("td")[0].find("p").find("a").string).strip()',
-		"location": 'unicode(item.find("div", {"class": "line-content-details"}).find_all("td")[3].find("p").string).replace("CLUJ-NAPOCA,", "").strip()',
-		"price": 'unicode(item.find("div", {"class": "line-content-details"}).find_all("td")[1].find("p").find("font").string).strip()',
-		"next_url": 'http://www.edil.ro/actions/cautare_avansata.php?loc=CJ_00&tip_imob=1&tip_tranzactie=2&cam_min=1&cam_max=2&pret_min=150&pret_max=200&oferte_recomandate=0&exclus_demisol=demisol&exclus_parter=parter&exclus_mansarda=&exclus_ultim_etaj=ultim_etaj&st_limit=%d&l=ro&monitorizare=1',
-	},
-	"chirii": {
-		"start_url": 'http://www.chirii-cluj.ro/cautare?type=a1+a2&hood=1+19+26+48+49+32+50+51+52&price-min=150&price-max=200',
-		"items": 'soup.find_all("li", {"class": "listing"})',
-		"url": '"http://www.chirii-cluj.ro" + item.find("h2").find("a")["href"]',
-		"name": 'unicode(item.find("h2").find("a").string).strip()',
-		"location": 'unicode("")',
-		"price": 'unicode(item.find("div", {"class": "property_price"}).find("a").string).replace(u"Price: €", "").replace("/month", "").strip()',
-		"next_url": '"http://www.chirii-cluj.ro" + soup.find("li", {"class": "page selected"}).next_sibling.next_sibling.find("a")["href"]',
-	},
-}
+from rent_conf import *
 
 #================================================================================================== GET DATA
 
@@ -100,6 +66,41 @@ def get_data(site, results, new_urls, paginate=True):
 
 #================================================================================================== LOAD, FETCH, SAVE, BUILD REPORT, SLEEP
 
+
+def sendemail(from_addr, to_addr_list,
+              subject, message,
+              login=None, password=None,
+              smtpserver='smtp.gmail.com:587'):
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = from_addr
+    msg['To'] = ','.join(to_addr_list)
+
+    # Record the MIME types of both parts - text/plain and text/html.
+    part1 = MIMEText('this is a text only preview, does your email support html ?', 'plain')
+    part2 = MIMEText(message, 'html')
+
+    # Attach parts into message container.
+    # According to RFC 2046, the last part of a multipart message, in this case
+    # the HTML message, is best and preferred.
+    msg.attach(part1)
+    msg.attach(part2)
+
+    server = smtplib.SMTP(smtpserver)
+    if login and password:
+        server.starttls()
+        server.login(login,password)
+    problems = server.sendmail(from_addr, to_addr_list, msg.as_string())
+    server.quit()
+
+def email_report(to=EMAIL_TO, html_file='zeh_report.html'):
+    message = open(html_file).read()
+    sendemail(from_addr='nemutam@gmail.com', to_addr_list=to,
+              subject='Ne mutam!', message=message,
+              smtpserver=SMTP_SERVER)
+
+
 while True:
 	if not os.path.isfile("results.pickle"):
 		results = {}
@@ -131,6 +132,9 @@ while True:
 		f.write(s)
 		f.close()
 		os.system("cat report_head.html results.html report_body.html > zeh_report.html")
+
+		email_report()
+
 		os.system('zenity --warning --title="rent-watcher alert" --text="Update(s) available" &')
 	else:
 		os.system('zenity --info --title="rent-watcher status" --text="Run ok, nothing new" &')
