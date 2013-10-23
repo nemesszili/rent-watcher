126a2399d3c9d69e39fe5ea3da500f9cfd397787
Viorel Stirbu <viorels@gmail.com>
2013-10-23 21:54:53 +0300

    convert tabs to spaces, changed from email address, mobile link for piata

--- rent-watcher.py
+++ rent-watcher.py
@@ -18,55 +18,55 @@ from rent_conf import *
 #================================================================================================== GET DATA
 
 def get_data(site, results, new_urls, paginate=True):
-	time = datetime.datetime.now()
-	urls = [config[site]["start_url"]]
-	last_url = config[site]["start_url"]
-	page = 0
-	while len(urls) > 0:
-		url = urls.pop()
-		print url
+    time = datetime.datetime.now()
+    urls = [config[site]["start_url"]]
+    last_url = config[site]["start_url"]
+    page = 0
+    while len(urls) > 0:
+        url = urls.pop()
+        print url
 
-		r = requests.get(url)
-		if url != config[site]["start_url"]:
-			if r.text == last_page:
-				break
-		last_page = r.text
+        r = requests.get(url)
+        if url != config[site]["start_url"]:
+            if r.text == last_page:
+                break
+        last_page = r.text
 
-		soup = bs4.BeautifulSoup(r.text, "html5lib")
-		items = eval(config[site]["items"])
-		print "  %d items" % len(items)
+        soup = bs4.BeautifulSoup(r.text, "html5lib")
+        items = eval(config[site]["items"])
+        print "  %d items" % len(items)
 
-		new = 0
-		for item in items:
-			url = eval(config[site]["url"])
-			name = eval(config[site]["name"])
-			location = eval(config[site]["location"])
-			price = eval(config[site]["price"])
-			if url not in results:
-				new += 1
-				results[url] = {
-					"name": name,
-					"location": location,
-					"price": price,
-					"time": time,
-				}
-				new_urls.append(url)
-		print "  %d new" % new
+        new = 0
+        for item in items:
+            url = eval(config[site]["url"])
+            name = eval(config[site]["name"])
+            location = eval(config[site]["location"])
+            price = eval(config[site]["price"])
+            if url not in results:
+                new += 1
+                results[url] = {
+                    "name": name,
+                    "location": location,
+                    "price": price,
+                    "time": time,
+                }
+                new_urls.append(url)
+        print "  %d new" % new
 
-		if paginate:
-			page += 1
-			try:
-				if site.find("edil") != -1:
-					next_url = config[site]["next_url"] % page
-				else:
-					next_url = eval(config[site]["next_url"])
-			except Exception:
-				next_url = None
-			if next_url != None and next_url != last_url:
-				urls.append(next_url)
-				last_url = next_url
+        if paginate:
+            page += 1
+            try:
+                if site.find("edil") != -1:
+                    next_url = config[site]["next_url"] % page
+                else:
+                    next_url = eval(config[site]["next_url"])
+            except Exception:
+                next_url = None
+            if next_url != None and next_url != last_url:
+                urls.append(next_url)
+                last_url = next_url
 
-	return (results, new_urls)
+    return (results, new_urls)
 
 #================================================================================================== LOAD, FETCH, SAVE, BUILD REPORT, SLEEP
 
@@ -100,47 +100,50 @@ def sendemail(from_addr, to_addr_list,
 
 def email_report(to=EMAIL_TO, html_file='zeh_report.html'):
     message = open(html_file).read()
-    sendemail(from_addr='nemutam@gmail.com', to_addr_list=to,
+    sendemail(from_addr='nemutam@stirbu.name', to_addr_list=to,
               subject='Ne mutam!', message=message,
               smtpserver=SMTP_SERVER)
 
 
 while True:
-	if not os.path.isfile("results.pickle"):
-		results = {}
-	else:
-		results = pickle.load(open("results.pickle", "rb"))
-		# sorted_results = sorted([url, info for url, info in results.items()]
-		# 						key=lambda url_info: url_info[1].get('time', None))
-	new_urls = []
+    if not os.path.isfile("results.pickle"):
+        results = {}
+    else:
+        results = pickle.load(open("results.pickle", "rb"))
+        # sorted_results = sorted([url, info for url, info in results.items()]
+        #                         key=lambda url_info: url_info[1].get('time', None))
+    new_urls = []
 
-	for site in config:
-		results, new_urls = get_data(site, results, new_urls)
-		pickle.dump(results, open("results.pickle", "wb"))
+    for site in config:
+        results, new_urls = get_data(site, results, new_urls)
+        pickle.dump(results, open("results.pickle", "wb"))
 
-	if len(new_urls) != 0:
-		now_str = str(datetime.datetime.now())[:19]
+    if len(new_urls) != 0:
+        now_str = str(datetime.datetime.now())[:19]
 
-		offers = [{'url': url,
-				   'name': results[url]['name'],
-				   'location': results[url]['location'],
-				   'price': results[url]['price'],
-				   } for url in new_urls]
-		context = {'date': str(datetime.datetime.now())[:19],
-		           'offers': offers}
-		
-		r = pystache.Renderer()
-		report = r.render_path('report_tmpl.html', context)
-		with codecs.open('zeh_report.html', 'w', encoding='utf-8') as report_f:
-			report_f.write(report)
+        mobile_url = lambda url: (url.replace('www', 'm').replace('oras-cluj-napoca/', '')
+                                  if 'piata-az' in url
+                                  else url)
+        offers = [{'url': mobile_url(url),
+                   'name': results[url]['name'],
+                   'location': results[url]['location'],
+                   'price': results[url]['price'],
+                   } for url in new_urls]
+        context = {'date': str(datetime.datetime.now())[:19],
+                   'offers': offers}
+        
+        r = pystache.Renderer()
+        report = r.render_path('report_tmpl.html', context)
+        with codecs.open('zeh_report.html', 'w', encoding='utf-8') as report_f:
+            report_f.write(report)
 
-		email_report()
+        email_report()
 
-		os.system('zenity --warning --title="rent-watcher alert" --text="Update(s) available" &')
-	# else:
-	# 	os.system('zenity --info --title="rent-watcher status" --text="Run ok, nothing new" &')
+        os.system('zenity --warning --title="rent-watcher alert" --text="Update(s) available" &')
+    # else:
+    #     os.system('zenity --info --title="rent-watcher status" --text="Run ok, nothing new" &')
 
-	print "Going to bed... ZzZzZzZzZzZzzz"
-	time.sleep(666)
+    print "Going to bed... ZzZzZzZzZzZzzz"
+    time.sleep(666)
 
 #==================================================================================================
